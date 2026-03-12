166f1aac006bdb098db252a2914506cecdd11aa5
Vedanthdamn <vedanthdama@gmail.com>
2026-03-12 14:26:36 +0530

    UC2: Basic Room Types & Static Availability

---
+++ src/UseCase2RoomInitialization.java
@@ -0,0 +1,58 @@
+abstract class Room {
+
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() { super(1, 250, 1500.0); }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() { super(2, 400, 2500.0); }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() { super(3, 750, 5000.0); }
+}
+
+public class UseCase2RoomInitialization {
+
+    public static void main(String[] args) {
+
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom  = new SuiteRoom();
+
+        int singleRoomAvailable = 5;
+        int doubleRoomAvailable = 3;
+        int suiteRoomAvailable  = 2;
+
+        System.out.println("Hotel Room Initialization");
+
+        System.out.println("\nSingle Room:");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available: " + singleRoomAvailable);
+
+        System.out.println("\nDouble Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleRoomAvailable);
+
+        System.out.println("\nSuite Room:");
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available: " + suiteRoomAvailable);
+    }
+}
